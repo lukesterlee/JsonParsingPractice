2b7ece9bfe82011ccf50e177c5e3b85c92aca3ba
Luke Desktop <lukesterlee@gmail.com>
2015-06-15 11:48:23 -0400

    initial commit

---
+++ src/nyc/c4q/lukesterlee/Main.java
@@ -0,0 +1,11 @@
+package nyc.c4q.lukesterlee;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+
+
+    }
+}
---
+++ src/nyc/c4q/lukesterlee/Practice1Flickr.java
@@ -0,0 +1,7 @@
+package nyc.c4q.lukesterlee;
+
+/**
+ * Created by Luke Lee on 6/15/2015.
+ */
+public class Practice1Flickr {
+}
